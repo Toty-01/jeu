e28a26582118550ab8d163dc5d7ec461c880033b
Toty-01 <caucheanthony@gmail.com>
2022-06-07 21:33:11 +0200

    refonte css(responsive) / html + autre approche js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// Player name
+// Initialisation des variables
+let playerNumber, activePlayer, scores, diceScore, inGame, currentScores;
+
 var player1 = "Player 1";
 var player2 = "Player 2";
 
@@ -6,16 +8,24 @@ var player2 = "Player 2";
 function editNames() {
     player1 = prompt("Change Player1 name");
     player2 = prompt("Change player2 name");
-
+   
     document.querySelector("h2.Player1").innerHTML = player1;
     document.querySelector("h2.Player2").innerHTML = player2;
 }
-// Function to roll dice
-function rollTheDice() {
-  setTimeout(function () {
-      var randomNumber = Math.floor(Math.random() * 6) + 1;
+// RAZ
+function newGame() {
+  scores = [0, 0];
+  currentScores = [0, 0];
+  activePlayer = 0;
+  diceScore = 0;
+  inGame = true;
+  playerNumber = [1, 2];
 
-      document.querySelector(".img-dice").setAttribute("src",
-          "./img/dice" + randomNumber + ".png");
-  }, 100);
+  document.getElementById('img-result').src = './img/dice1.png';
+  document.getElementById('score-player-1').textContent = 0;
+  document.getElementById('score-player-2').textContent = 0;
+  document.getElementById('dice-result-1').textContent = 0;
+  document.getElementById('dice-result-2').textContent = 0;
+  document.getElementById('select-player-1').style.color = 'white';
+  document.getElementById('select-player-2').style.color = 'black';
 }
